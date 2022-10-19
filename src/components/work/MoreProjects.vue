355c4f2aa0969d4bce488d219d43b69dfee0010b
Juan Luna <juanluna@Juans-MacBook-Pro.local>
2022-10-19 15:58:20 -0500

    Ajustes work

--- src/components/work/MoreProjects.vue
+++ src/components/work/MoreProjects.vue
@@ -24,8 +24,8 @@
             <div class="container">
               <div class="text text--small">
                 <div v-html="project.excerpt.rendered"></div>
-                <a :href="project.acf.project_link.url" class="link">
-                  Visit Website
+                <a :href="project.acf.project_link.url" class="link link--light">
+                  Visit website
                 </a>
               </div>
             </div>
