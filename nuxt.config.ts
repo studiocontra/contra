1d70cc5aa72935adc3b5e25e2dfa7b9936ff84c0
Josneth Moreno <josnethmoreno@gmail.com>
2023-05-30 18:11:07 -0400

    add delay hidratation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,12 @@ export default defineNuxtConfig({
         lastmod: new Date().toISOString(),
       },
     }],
+    'nuxt-delay-hydration',
   ],
+  delayHydration: {
+    debug: process.env.NODE_ENV === 'development',
+    
+  },
  
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
