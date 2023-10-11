5949b785b264fdb0ea8aeeae2e8752660630500b
Josneth Moreno <josnethmoreno@gmail.com>
2023-10-11 10:49:59 -0400

    update media link work projects

--- src/components/work/MoreProjects.vue
+++ src/components/work/MoreProjects.vue
@@ -11,7 +11,7 @@
               <div class="col-md-6">
                 <div class="tags">
                   <span v-for="(cat, idx) in project.categories"
-                  :key="idx" v-html="cat.name"></span>
+                  :key="idx">{{ (idx > 0) ? cat.name : `${cat.name},`}}</span>
                 </div>
               </div>
 
